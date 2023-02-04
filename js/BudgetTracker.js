a2d2d006127165956e39dde2af2ce159e29d0dd5
unknown <prashanthmanohar.g@outlook.com>
2023-02-04 15:32:27 +0530

    added javascript functions for budget tracker

---
+++ js/BudgetTracker.js
@@ -0,0 +1,66 @@
+export default class BudgetTracker{
+
+constructor(querySelectorString)
+{
+
+
+
+}
+
+static html()
+{
+
+
+}
+
+static entryHtml()
+{
+
+
+
+}
+
+load()
+{
+
+
+}
+
+updateSummary()
+{
+
+
+}
+
+save()
+{
+
+
+}
+
+addEntry(entry = {})
+{
+
+
+}
+
+getEntryRows()
+{
+
+
+}
+
+onNewEntryBtnClick()
+{
+
+
+}
+
+onDeleteEntryBrnClick(e) {
+
+
+    
+}
+
+
+}
