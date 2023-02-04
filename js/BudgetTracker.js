a32e89909bab7428c792f382efe17f8a0be08a1e
unknown <prashanthmanohar.g@outlook.com>
2023-02-04 16:04:28 +0530

    added javascript implementation for loading the page

--- js/BudgetTracker.js
+++ js/BudgetTracker.js
@@ -2,14 +2,49 @@ export default class BudgetTracker{
 
 constructor(querySelectorString)
 {
+this.root = document.querySelector(querySelectorString);
+this.root.innerHTML = BudgetTracker.html();
 
+this.root.querySelector(".new-entry").addEventListener("click",() => {
+this.onNewEntryBtnClick();
 
+});
+
+//Load initial data from Local Storage
+this.load();
 
 }
 
 static html()
 {
+  return `
+  <table class="budget-tracker">
+    <thead>
+    <tr>
+      <th>Date</th>
+      <th>Description</th>
+      <th>Type</th>
+      <th>Amount</th>
+    <th></th>
+    </tr>
+    </thead>
 
+    <tbody class="entries"></tbody>
+
+    <tfoot>
+         <tr>
+             <td colspan="5" class="summary">
+                  <strong>Total: </strong>
+                  <span class="total">0.0 Rs</span>
+
+             </td>
+
+         </tr>
+
+    </tfoot>
+
+</table>
+  `
 
 }
 
@@ -59,7 +94,7 @@ onNewEntryBtnClick()
 onDeleteEntryBrnClick(e) {
 
 
-    
+
 }
 
 
